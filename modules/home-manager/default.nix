c91862d0f3b543989ac4af50e68b4246fe3db5f0
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-06-13 07:10:25 +0200

    added zsh-vi-mode

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,5 @@
 {
   # List module files here
   # my-module = import ./my-module.nix;
+  zvm = import ./terminal/zvm.nix;
 }
---
+++ modules/home-manager/terminal/zvm.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  programs = {
+    zsh = {
+      enable = true;
+      plugins = [
+        {
+          name = "vi-mode";
+          src = pkgs.zsh-vi-mode;
+          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        }
+      ];
+    };
+  };
+}
