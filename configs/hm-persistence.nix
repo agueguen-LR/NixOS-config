2d8776847a885c3eaeecb1726a335c006a155b07
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-06-25 20:25:48 +0200

    persist Steam data

--- configs/hm-persistence.nix
+++ configs/hm-persistence.nix
@@ -2,6 +2,10 @@
   home.persistence."/persist/adrien" = {
     directories = [
       ".local/share/soh" #Ship of harkinian
+      {
+        directory = ".local/share/Steam";
+        method = "symlink";
+      }
     ];
     files = [
       ".gitconfig"
