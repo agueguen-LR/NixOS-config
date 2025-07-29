862ced2983fa49e128923ebb475612f1154240bb
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-07-29 20:27:15 +0200

    added pc host

---
+++ hosts/pc.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+	imports = [
+		../hardware/pc-hardware.nix
+		../disko/pc-disko.nix
+	];
+
+	networking = {
+		hostName = "nixos-pc";
+    hostId = "466b81e1"; # Needed for ZFS
+	};
+}
