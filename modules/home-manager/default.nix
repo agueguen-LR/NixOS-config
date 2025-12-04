ceefa967695bbb363b637429c4eef7142397130d
agueguen-LR <adrien.gueguen@etudiant.univ-lr.fr>
2025-12-04 20:53:26 +0100

    Better terminal hm directories

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,13 +1,13 @@
 {
   # List module files here, alphabetically please :)
   # my-module = import ./my-module.nix;
-  alacritty = import ./terminal/alacritty.nix;
+  alacritty = import ./terminal/terminal-emulator/alacritty.nix;
   catppuccin = import ./style/catppuccin.nix;
   direnv = import ./devEnvironments/direnv.nix;
-  fish = import ./terminal/fish.nix;
+  fish = import ./terminal/shell/fish.nix;
   fuzzel = import ./appLauncher/fuzzel.nix;
-  fzf = import ./terminal/fzf.nix;
-  kitty = import ./terminal/kitty.nix;
+  fzf = import ./terminal/utilities/fzf.nix;
+  kitty = import ./terminal/terminal-emulator/kitty.nix;
   librewolf = import ./browser/librewolf.nix;
   hyprland = import ./compositor/hyprland;
   hyprpaper = import ./wallpaper/hyprpaper.nix;
@@ -21,10 +21,10 @@
   plasma = import ./desktopEnvironment/plasma.nix;
   stylix = import ./style/stylix.nix;
   swww = import ./wallpaper/swww.nix;
-  tmux = import ./terminal/tmux.nix;
+  tmux = import ./terminal/utilities/tmux.nix;
   rofi = import ./appLauncher/rofi.nix;
   waybar = import ./statusBar/waybar.nix;
   yazi = import ./fileManager/yazi.nix;
-  zoxide = import ./terminal/zoxide.nix;
-  zsh = import ./terminal/zsh.nix;
+  zoxide = import ./terminal/utilities/zoxide.nix;
+  zsh = import ./terminal/shell/zsh.nix;
 }
